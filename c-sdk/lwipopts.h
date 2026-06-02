0e3bf2c9c9c499e623be2020f4cc7f0385f634a0
Sriram-Gutta <ram.gutta65@gmail.com>
2026-06-01 23:30:40 -0700

    Use canonical lwIP options - undersized pbuf pool was failing WPA2
    My hand-trimmed lwipopts had PBUF_POOL_SIZE=4 and a tiny MEM_SIZE.
    Worked for the simple TCP server, but once the DHCP server added
    UDP traffic, pbufs ran out mid WPA2 four-way handshake. Clients
    saw 'couldn't connect to network' with no obvious cause.
    
    Replaced with the same lwipopts_examples_common.h values that the
    official pico-examples AP demo ships with: PBUF_POOL_SIZE=24,
    MEM_SIZE=4000, larger TCP windows, ARP queue, etc.
    
    The footprint cost is a few KB of RAM; the binary still fits
    easily in the Pico 2W's flash.

--- c-sdk/lwipopts.h
+++ c-sdk/lwipopts.h
@@ -1,24 +1,54 @@
+// lwIP options for the Pico W AP + HTTP server.
+// Lifted from the canonical pico-examples settings:
+// https://github.com/raspberrypi/pico-examples/blob/master/pico_w/wifi/lwipopts_examples_common.h
+//
+// The earlier hand-trimmed version had PBUF_POOL_SIZE=4 and a tiny MEM_SIZE.
+// That ran out of pbufs during the WPA2 4-way handshake once the DHCP server
+// was added, which made the client see "couldn't connect to network".
+// These pool sizes are the values the official AP example ships with.
+
 #ifndef __LWIPOPTS_H__
 #define __LWIPOPTS_H__
 
-/* Minimal lwip options for Pico W webserver */
-#define NO_SYS                          1
-#define MEM_ALIGNMENT                   4
-#define LWIP_RAW                        0
-#define LWIP_NETCONN                    0
-#define LWIP_SOCKET                     0
-#define LWIP_DHCP                       0
-#define LWIP_ICMP                       1
-#define LWIP_UDP                        1
-#define LWIP_TCP                        1
-#define LWIP_IPV4                       1
-#define LWIP_IPV6                       0
-#define ETH_PAD_SIZE                    0
+#define NO_SYS                      1
+#define LWIP_SOCKET                 0
 
-#define TCP_MSS                         (1500 - 20 - 20)
-#define TCP_SND_BUF                     (4 * TCP_MSS)
-#define TCP_WND                         (4 * TCP_MSS)
+#if PICO_CYW43_ARCH_POLL
+#define MEM_LIBC_MALLOC             1
+#else
+#define MEM_LIBC_MALLOC             0
+#endif
 
-#define PBUF_POOL_SIZE                  4
+#define MEM_ALIGNMENT               4
+#define MEM_SIZE                    4000
+#define MEMP_NUM_TCP_SEG            32
+#define MEMP_NUM_ARP_QUEUE          10
+#define PBUF_POOL_SIZE              24
 
-#endif /* __LWIPOPTS_H__ */
+#define LWIP_ARP                    1
+#define LWIP_ETHERNET               1
+#define LWIP_ICMP                   1
+#define LWIP_RAW                    1
+#define LWIP_IPV4                   1
+#define LWIP_TCP                    1
+#define LWIP_UDP                    1
+#define LWIP_DNS                    1
+#define LWIP_DHCP                   1
+#define LWIP_NETCONN                0
+
+#define TCP_MSS                     1460
+#define TCP_WND                     (8 * TCP_MSS)
+#define TCP_SND_BUF                 (8 * TCP_MSS)
+#define TCP_SND_QUEUELEN            ((4 * (TCP_SND_BUF) + (TCP_MSS - 1)) / (TCP_MSS))
+#define LWIP_TCP_KEEPALIVE          1
+
+#define LWIP_NETIF_STATUS_CALLBACK  1
+#define LWIP_NETIF_LINK_CALLBACK    1
+#define LWIP_NETIF_HOSTNAME         1
+#define LWIP_NETIF_TX_SINGLE_PBUF   1
+
+#define DHCP_DOES_ARP_CHECK         0
+#define LWIP_DHCP_DOES_ACD_CHECK    0
+#define LWIP_CHKSUM_ALGORITHM       3
+
+#endif // __LWIPOPTS_H__
